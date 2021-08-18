1d01dd0eec7d0ff57de9fa8c1a2551a22432a652
DoJiSeon <djs6620@gmail.com>
2021-08-18 23:58:43 +0900

    sqlite 데이터 업데이트, 조회 구현

--- app/src/main/java/com/bliss/csc/dbtest4/DBManager.java
+++ app/src/main/java/com/bliss/csc/dbtest4/DBManager.java
@@ -4,38 +4,42 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.Toast;
 
 public class DBManager extends SQLiteOpenHelper {
 
+    private static final String TABLE_COLUMN_AVERAGE = "average";
+    private static final String TABLE_NAME = "Android";
     public static final String DB_NAME = "dbtest.db";
     public static final int DB_VERSION = 1;
-    private  SQLiteDatabase mDB;
+    private SQLiteDatabase mDB;
     private String str;
-    private int temp;
+    private int count_ID = 1;
     public DBManager(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-         db.execSQL("CREATE TABLE Android( _id INTEGER PRIMARY KEY AUTOINCREMENT," + "grade INTEGER, semester INTEGER, name TEXT, price INTEGER);"); //name = 중간, 기말, price = 평균
-        db.execSQL("INSERT INTO Android VALUES (null, 1, 2, '중간고사' , 90 );");
+         db.execSQL("CREATE TABLE Android( _id INTEGER ," + "grade INTEGER, semester INTEGER, exam TEXT, average INTEGER);"); //name = 중간, 기말, price = 평균
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("INSERT INTO Android VALUES (null, 1, 2, '기말고사' , 98 );");
-
+        // db.execSQL("INSERT INTO Android VALUES (null, 1, 2, '기말고사' , 98 );");
+        db.execSQL("DROP TABLE IF EXISTS Android");
+        onCreate(db);
     }
 
     @Override public void onOpen(SQLiteDatabase db) { super.onOpen(db); mDB = db; }
 
-    public void insert(DBManager mDBManager,Integer grade, Integer semester, String name, Integer average)
+    public void insert(DBManager mDBManager,Integer grade, Integer semester, String exam, Integer average)
     {
         mDBManager.getWritableDatabase();
-    mDB.execSQL("INSERT INTO Android VALUES (null, "+ grade +", " +semester+", '"+ name +"', "+ average +" );");
+    mDB.execSQL("INSERT INTO Android VALUES (" + count_ID + ", "+ grade +", " +semester+", '"+ exam +"', "+ average +" );");
     mDBManager.close();
-    String insert = grade + " ,"+ semester+" ," + name + ", " + average;
+    String insert = grade + " ,"+ semester+" ," + exam + ", " + average;
+    count_ID ++;
     Log.i("Insert DB : ", insert);
     }
 
@@ -45,17 +49,21 @@ public class DBManager extends SQLiteOpenHelper {
         mDB.execSQL("DELETE FROM Android");
         mDBManager.close();
         Log.i("Delete DB", "모든 테이블이 삭제 되었습니다.");
+        count_ID = 1;
     }
 
-    public void update(DBManager mDBManager)
-    { mDBManager.getWritableDatabase();
-    mDB.execSQL("UPDATE Android SET price = "+ (temp+10) +" WHERE price = "+ (temp-1) +";");
-    mDBManager.close();
-    Log.v(null,"Update DB : "+temp);
+    public void update(DBManager mDBManager, Integer id, Integer grade, Integer semester, String exam, Integer average)
+    {
+        mDBManager.getWritableDatabase();
+        // mDB.execSQL("UPDATE Android SET price = "+ (count_ID+10) +" WHERE price = "+ (count_ID-1) +";");
+        mDB.execSQL("INSERT OR REPLACE INTO Android(_id, grade, semester, exam, average) VALUES (" + id+ ", "+ grade +", " +semester+", '"+ exam +"', "+ average +" );");
+        mDBManager.close();
+        Log.v(null,"Update DB : "+count_ID);
     }
 
-    public void select(DBManager mDBManager) {
-
+    public void all_average(DBManager mDBManager) {
+        String query = "SELECT AVG("+ TABLE_COLUMN_AVERAGE +") FROM "+ TABLE_NAME;
+        mDB.rawQuery(query, null);
     }
 
 }
--- app/src/main/java/com/bliss/csc/dbtest4/MainActivity.java
+++ app/src/main/java/com/bliss/csc/dbtest4/MainActivity.java
@@ -2,14 +2,19 @@ package com.bliss.csc.dbtest4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteConstraintException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,44 +23,69 @@ public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase mDB;
 
     Button btn_insert,btn_delete,btn_update,btn_select;
-    EditText grade, semester, average;
+    EditText grade, semester, average, id;
+    ListView listView, listView2, listView3, listView4, listView_id;
+    Cursor cursor;
+    ArrayAdapter adapter, adapter2, adapter3, adapter4, adapter_id;
     RadioGroup examgroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         grade = (EditText)findViewById(R.id.grade);
         semester = (EditText)findViewById(R.id.semester);
         average = (EditText)findViewById(R.id.average);
+        id = (EditText)findViewById(R.id.id_edt);
 
         btn_insert = (Button)findViewById(R.id.btn_insert);
         btn_delete = (Button)findViewById(R.id.btn_delete);
         btn_update = (Button)findViewById(R.id.btn_update);
         btn_select = (Button)findViewById(R.id.btn_select);
 
+        listView = (ListView)findViewById(R.id.listview);
+        listView2 = (ListView)findViewById(R.id.listview2);
+        listView3 = (ListView)findViewById(R.id.listview3);
+        listView4 = (ListView)findViewById(R.id.listview4);
+        listView_id = (ListView)findViewById(R.id.listview_id);
+
+
         examgroup = (RadioGroup)findViewById(R.id.examRgroup);
+
         mDBManager = new DBManager(this);
         mDBManager.getReadableDatabase();
         mDBManager.getWritableDatabase();
         mDBManager.close();
 
+
         btn_insert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Integer Grade = Integer.valueOf(grade.getText().toString());
-                Integer Semester = Integer.valueOf(semester.getText().toString());
-                Integer Average = Integer.valueOf(average.getText().toString());
-                String exam;
-                int rbt_id = ((RadioGroup)examgroup.findViewById(R.id.examRgroup)).getCheckedRadioButtonId();
-                if (rbt_id == 2131231195){ // 중간고사 버튼 클릭 시!
-                    exam = "중간고사";
-                }else {
-                    exam = "기말고사";
+
+                try {
+                    Integer Grade = Integer.valueOf(grade.getText().toString());
+                    Integer Semester = Integer.valueOf(semester.getText().toString());
+                    Integer Average = Integer.valueOf(average.getText().toString());
+                    String exam;
+                    int rbt_id = ((RadioGroup)examgroup.findViewById(R.id.examRgroup)).getCheckedRadioButtonId();
+                    if (rbt_id == 2131231195){ // 중간고사 버튼 클릭 시!
+                        exam = "중간고사";
+                    }else {
+                        exam = "기말고사";
+                    }
+                    String rbt_result = String.valueOf(rbt_id);
+                    Log.i("라디오 버튼",rbt_result);
+                    try {
+                        mDBManager.insert(mDBManager, Grade, Semester, exam, Average);
+                        grade.setText("");
+                        semester.setText("");
+                        average.setText("");
+                    }catch (SQLiteConstraintException e){
+                        Toast.makeText(getApplicationContext(),"시험 종류를 선택해주세요",Toast.LENGTH_SHORT).show();
+                    }
+
+                }catch (NumberFormatException e){
+                    Toast.makeText(getApplicationContext(),"모든 칸과 버튼을 채워주세요",Toast.LENGTH_SHORT).show();
                 }
-                String rbt_result = String.valueOf(rbt_id);
-                Log.i("라디오 버튼",rbt_result);
-                mDBManager.insert(mDBManager, Grade, Semester, exam, Average);
 
             }
         });
@@ -64,22 +94,81 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mDBManager.delete(mDBManager);
+                Toast.makeText(getApplicationContext(),"모든 데이터가 삭제되었습니다.",Toast.LENGTH_SHORT).show();
             }
+
         });
 
         btn_update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mDBManager.update(mDBManager);
+
+                try {
+                    Integer ID = Integer.valueOf(id.getText().toString());
+                    Integer Grade = Integer.valueOf(grade.getText().toString());
+                    Integer Semester = Integer.valueOf(semester.getText().toString());
+                    Integer Average = Integer.valueOf(average.getText().toString());
+                    String exam;
+                    int rbt_id = ((RadioGroup)examgroup.findViewById(R.id.examRgroup)).getCheckedRadioButtonId();
+                    if (rbt_id == 2131231195){ // 중간고사 버튼 클릭 시!
+                        exam = "중간고사";
+                    }else {
+                        exam = "기말고사";
+                    }
+                    mDBManager.update(mDBManager, ID, Grade, Semester, exam , Average);
+
+
+                }catch (NumberFormatException e){
+                    Toast.makeText(getApplicationContext(), "수정하고 싶다면 모든 값을 채워주세요.",Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         btn_select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mDBManager.select(mDBManager);
+                //mDBManager.select(mDBManager);
+                listUpdate();
             }
         });
 
     }
+
+    public Cursor avg_average(){
+        //String query = "SELECT AVG("+ TABLE_COLUMN_AVERAGE +") FROM "+ TABLE_NAME;
+
+        mDB = mDBManager.getReadableDatabase();
+        Cursor cursor = mDB.rawQuery("SELECT AVG( average ) FROM Android;", null);
+        startManagingCursor(cursor);
+        cursor.close();
+        return cursor;
+        
+    }
+
+    public void listUpdate(){
+        mDB = mDBManager.getReadableDatabase();
+        cursor = mDB.rawQuery("SELECT * FROM Android",null);
+        startManagingCursor(cursor);
+
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        adapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        adapter3 = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        adapter4 = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        adapter_id = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+
+        while (cursor.moveToNext()){
+            adapter_id.add(cursor.getInt(0));
+            adapter.add(cursor.getInt(1));
+            adapter2.add(cursor.getInt(2));
+            adapter3.add(cursor.getString(3));
+            adapter4.add(cursor.getInt(4));
+        }
+        listView_id.setAdapter(adapter_id);
+        listView.setAdapter(adapter);
+        listView2.setAdapter(adapter2);
+        listView3.setAdapter(adapter3);
+        listView4.setAdapter(adapter4);
+        cursor.close();
+    }
+
 }
